a5ee0343ee48f11031210cfaba91b3d84e87f006
RotaruT <168171163+RotaruT@users.noreply.github.com>
2024-05-03 16:25:10 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function mobile() {
+    var x = document.getElementsByTagName("navbar")[0];
+    if (x.className === "nav") {
+        x.className = "nav mobile";
+    } else {
+     x.className = "nav";
+    }
+
+}
